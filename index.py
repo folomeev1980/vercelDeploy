f008cf8b1220f0a082029b3606835511ca883115
folomeev1980 <a.a.folomeev80@gmail.com>
2022-09-21 17:48:56 +0300

    6 step

--- index.py
+++ index.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "<h2>Flask Vercel</h2>"
+
+
+@app.route("/other_route")
+def other():
+    return "<h2>Other Pagel</h2>"
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
