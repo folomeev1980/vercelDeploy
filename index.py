e6d038848df88344dec3a877ee2ec3ce1bc0d1f1
folomeev1980 <a.a.folomeev80@gmail.com>
2022-09-21 18:19:31 +0300

    7 step

--- index.py
+++ index.py
@@ -1,11 +1,25 @@
 from flask import Flask
+import time
+
+
+class Value:
+    val = 0
+
+    def valueCalculator(self):
+        while True:
+            Value.val = Value.val + 1
+            time.sleep(10)
+
 
 app = Flask(__name__)
+
+v = Value()
+v.valueCalculator()
 
 
 @app.route("/")
 def home():
-    return "<h2>Flask Vercel</h2>"
+    return "<h2>Flask Vercel {} </h2>".format(v.val)
 
 
 @app.route("/other_route")
